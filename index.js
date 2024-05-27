1602ea3ffa18a59c50fb49bc61ea7998a8c68b41
Yogesh-88 <ydasi793@gmail.com>
2024-05-27 17:16:41 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin:
+    "https://rentify-frontend-cfmt-6zjypaqbh-yogesh-88s-projects.vercel.app", // Frontend URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
